0719e4167cc2ee99285cd943f827c9d4ddb562bf
yohanis-ke <yohanes.ke@gmail.com>
2018-09-12 15:30:06 -0400

    02 - Light Switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+  var light = document.querySelector('.switch')
+  var body = document.querySelector('body')
+  var status = document.querySelector('.status')
+  status.innerHTML = 'so bright'
 
-
+  light.addEventListener('click', function(e) {
+    if (light.className == 'switch on') {
+    e.target.className = 'switch off'
+    body.className = 'dark'
+    status.innerHTML = '<span>daaarkness</span>'
+  }
+    else {
+      light.className = 'switch on'
+      body.className = 'light'
+      status.innerHTML = 'so bright'
+    }
+  })
+})
